74bef54879550ce613718a8a75d14d7f5799755c
hexNulgath <machadoignacio@hotmail.com>
2025-04-04 20:54:37 -0300

    Refactor canUnlockAll function to improve logic and efficiency

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,19 +8,20 @@ def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
     Each box contains a list of keys to other boxes.
-    The function uses a breadth-first search approach to explore the boxes.
     """
     n = len(boxes)
     if n == 0:
-        return False
+        return True
 
-    keys = list(boxes[0])
     unlocked = {0}
+    to_process = [0]
 
-    while keys:
-        current_key = keys.pop(0)
-        if 0 <= current_key < n and current_key not in unlocked:
-            unlocked.add(current_key)
-            keys.extend(boxes[current_key])
+    while to_process:
+        current_box = to_process.pop()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and key not in unlocked:
+                unlocked.add(key)
+                to_process.append(key)
 
     return len(unlocked) == n
