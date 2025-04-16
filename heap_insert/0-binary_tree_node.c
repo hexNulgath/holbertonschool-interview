d8d448ab57d77385e022c30b981d904c4bbb11d2
hexNulgath <machadoignacio@hotmail.com>
2025-04-15 23:01:01 -0300

    Implement binary tree node creation and printing functions

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+    if (node == NULL)
+        return (NULL);
+
+    node->n = value;
+    node->parent = parent;
+    if (parent != NULL)
+    {
+        if (value < parent->n)
+            parent->left = node;
+        else
+            parent->right = node;
+    }
+    else
+    {
+        node->left = NULL;
+        node->right = NULL;
+    }
+
+    return (node);
+}
---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,7 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+}
