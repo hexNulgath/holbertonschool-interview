25c49ec924a7813f5996f5e403f716b8a1ef9d96
hexNulgath <machadoignacio@hotmail.com>
2025-04-15 23:15:43 -0300

    Implement binary tree node creation and add min_depth function for heap insertion

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,6 +1,13 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to put in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *node = malloc(sizeof(binary_tree_t));
@@ -10,17 +17,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
     node->n = value;
     node->parent = parent;
+    node->left = NULL;
+    node->right = NULL;
+
     if (parent != NULL)
     {
         if (value < parent->n)
             parent->left = node;
         else
             parent->right = node;
-    }
-    else
-    {
-        node->left = NULL;
-        node->right = NULL;
     }
 
     return (node);
--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -4,4 +4,35 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
+    heap_t *parent = NULL;
+    parent = min_depth(*root);
+    if (parent == NULL)
+    {
+        return (NULL);
+    }
+
+    binary_tree_node(*parent, int value);
+}
+
+heap_t *min_depth(heap_t *node)
+{
+    if (node == NULL)
+    {
+        return (NULL);
+    }
+    if (node->left == NULL && node->right == NULL)
+    {
+        return (node);
+    }
+    heap_t *left = min_depth(node->left);
+    heap_t *right = min_depth(node->right);
+    if (left == NULL)
+    {
+        return (right);
+    }
+    if (right == NULL)
+    {
+        return (left);
+    }
+    return (left->height < right->height ? left : right);
 }
