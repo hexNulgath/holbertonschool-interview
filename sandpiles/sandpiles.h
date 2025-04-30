ff543599f08a2fc5a852630a776aaa5135bd7da3
hexNulgath <machadoignacio@hotmail.com>
2025-04-30 14:58:18 -0300

    Fix header guard in sandpiles.h to prevent multiple inclusions

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,3 +1,6 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -5,3 +8,5 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 bool is_stable(int grid[3][3]);
 void sand_disperse(int grid[3][3]);
 void print(int grid[3][3]);
+
+#endif /* SANDPILES_H */
