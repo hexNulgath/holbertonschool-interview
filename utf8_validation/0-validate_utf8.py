bcd5765f1ff40a6728834ffcff60958ce089b78d
hexNulgath <machadoignacio@hotmail.com>
2025-06-06 01:18:51 -0300

    Implement strict validation for UTF-8 encoding in validUTF8 function

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -13,8 +13,23 @@ def validUTF8(data):
     Each integer represents 1 byte of data, therefore you only need to
     handle the 8 least significant bits of each integer
     """
-    for number in data:
-        if number < 0 or number > 255:
-            return False
+    num_bytes = 0
+    for byte in data:
+        byte &= 0xFF
 
+        if num_bytes == 0:
+            if (byte >> 7) == 0b0:
+                continue
+            elif (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
     return True
