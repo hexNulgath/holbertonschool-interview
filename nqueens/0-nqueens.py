87a61340f6d3bee121aa4869c98d292134cb04fc
hexNulgath <machadoignacio@hotmail.com>
2025-06-28 14:15:54 -0300

    Remove unnecessary blank line in 0-nqueens.py

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -77,15 +77,13 @@ if __name__ == "__main__":
         N = int(sys.argv[1])
         if type(N) is not int:
             raise TypeError
-        if N < 4:
-            raise ValueError
-    except ValueError:
-        print("N must be at least 4")
-        sys.exit(1)
     except TypeError:
         print("N must be a number")
         sys.exit(1)
-    
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
     solutions = nqueen(N)
     for solution in solutions:
