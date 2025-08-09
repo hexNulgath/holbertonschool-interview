248e86f4663cadaf646aa5be616f4e65d9c2ca29
Your Name <your.email@example.com>
2025-08-09 03:40:49 +0000

    Feat: Implement advanced binary search with recursive function and array printing

---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+void print_array(int *array, size_t start, size_t end);
+int advanced_binary_recursive(int *array, size_t start, size_t end, int value);
+int advanced_binary(int *array, size_t size, int value);
+#endif /* SEARCH_ALGOS */
