46014b2d6ff3e8466686c3c6036d5ce3a1e8d835
hexNulgath <machadoignacio@hotmail.com>
2025-10-09 12:53:04 -0300

    Refactor: Consolidate variable declarations in radix_sort function

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -7,24 +7,20 @@
  */
 void radix_sort(int *array, size_t size)
 {
-	int max, exp;
-	int *output;
+	int max, exp, *output;
 	size_t i;
 
 	if (!array || size < 2)
 		return;
-
 	output = malloc(sizeof(int) * size);
 	if (!output)
 		return;
-
 	max = array[0];
 	for (i = 1; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
 	}
-
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
 		int count[10] = {0};
@@ -33,24 +29,20 @@ void radix_sort(int *array, size_t size)
 		{
 			count[(array[i] / exp) % 10]++;
 		}
-
 		for (i = 1; i < 10; i++)
 		{
 			count[i] += count[i - 1];
 		}
-
 		for (i = size; i > 0; i--)
 		{
 			output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
 			count[(array[i - 1] / exp) % 10]--;
 		}
-
 		for (i = 0; i < size; i++)
 		{
 			array[i] = output[i];
 		}
 		print_array(array, size);
 	}
-
 	free(output);
 }
