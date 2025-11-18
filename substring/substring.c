4efafbd7e96da2e2d4ec8804dfe839bbccedf7e4
hexNulgath <machadoignacio@hotail.com>
2025-11-18 14:15:21 -0300

    refactor: improve readability and structure of substring search functions

--- substring/substring.c
+++ substring/substring.c
@@ -1,62 +1,75 @@
 #include "substring.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+bool find_substring_helper(char const *s, char const **words, int nb_words, int word_len);
 
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-    int word_len = strlen(words[0]);
-    const char *s_start = s;
+	int word_len = strlen(words[0]);
+	const char *s_start = s;
 
-    *n = 0;
-    int *arr = malloc(10000 * sizeof(int));
+	*n = 0;
+	int *arr = malloc(10000 * sizeof(int));
 
-    while (*s != '\0') {
+	while (*s != '\0') {
 
-        for (int i = 0; i < nb_words; i++) {
-            const char *word = words[i];
-            char const *helper[nb_words - 1];
-            int new_size = 0;
+		bool found = false;
 
-            for (int j = 0; j < nb_words; j++) {
-                if (j != i) {
-                    helper[new_size++] = words[j];
-                }
-            }
+		for (int i = 0; i < nb_words; i++) {
+			const char *word = words[i];
 
-            if (strncmp(s, word, word_len) == 0) {
-                if (find_substring_helper(s + word_len, helper, new_size, word_len)) {
-                    arr[(*n)++] = s - s_start;
-                }
-            }
-        }
+			if (strncmp(s, word, word_len) == 0) {
+				char const *helper[nb_words - 1];
+				int new_size = 0;
 
-        s++;
-    }
+				for (int j = 0; j < nb_words; j++) {
+					if (j != i) {
+						helper[new_size++] = words[j];
+					}
+				}
 
-    return arr;
+				if (find_substring_helper(s + word_len, helper, new_size, word_len)) {
+					found = true;
+					break;
+				}
+			}
+		}
+
+		if (found) {
+			arr[(*n)++] = s - s_start;
+		}
+
+		s++;
+	}
+
+	return arr;
 }
 
 bool find_substring_helper(char const *s, char const **words, int nb_words, int word_len)
 {
-    if (nb_words == 0)
-        return true;
+	if (nb_words == 0)
+		return true;
 
-    for (int i = 0; i < nb_words; i++) {
-        const char *word = words[i];
+	for (int i = 0; i < nb_words; i++) {
 
-        if (strncmp(s, word, word_len) == 0) {
+		const char *word = words[i];
 
-            char const *helper[nb_words - 1];
-            int new_size = 0;
+		if (strncmp(s, word, word_len) == 0) {
 
-            for (int j = 0; j < nb_words; j++) {
-                if (j != i)
-                    helper[new_size++] = words[j];
-            }
+			char const *helper[nb_words - 1];
+			int new_size = 0;
 
-            if (find_substring_helper(s + word_len, helper, new_size, word_len))
-                return true;
-        }
-    }
+			for (int j = 0; j < nb_words; j++) {
+				if (j != i)
+					helper[new_size++] = words[j];
+			}
 
-    return false;
+			if (find_substring_helper(s + word_len, helper, new_size, word_len))
+				return true;
+		}
+	}
+
+	return false;
 }
