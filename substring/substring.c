e925bd12c41a7ffd65b5394eb35f63a50233bc9a
hexNulgath <machadoignacio@hotail.com>
2025-11-18 14:02:34 -0300

    feat: implement substring search functionality with helper functions

---
+++ substring/substring.c
@@ -0,0 +1,62 @@
+#include "substring.h"
+
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+    int word_len = strlen(words[0]);
+    const char *s_start = s;
+
+    *n = 0;
+    int *arr = malloc(10000 * sizeof(int));
+
+    while (*s != '\0') {
+
+        for (int i = 0; i < nb_words; i++) {
+            const char *word = words[i];
+            char const *helper[nb_words - 1];
+            int new_size = 0;
+
+            for (int j = 0; j < nb_words; j++) {
+                if (j != i) {
+                    helper[new_size++] = words[j];
+                }
+            }
+
+            if (strncmp(s, word, word_len) == 0) {
+                if (find_substring_helper(s + word_len, helper, new_size, word_len)) {
+                    arr[(*n)++] = s - s_start;
+                }
+            }
+        }
+
+        s++;
+    }
+
+    return arr;
+}
+
+bool find_substring_helper(char const *s, char const **words, int nb_words, int word_len)
+{
+    if (nb_words == 0)
+        return true;
+
+    for (int i = 0; i < nb_words; i++) {
+        const char *word = words[i];
+
+        if (strncmp(s, word, word_len) == 0) {
+
+            char const *helper[nb_words - 1];
+            int new_size = 0;
+
+            for (int j = 0; j < nb_words; j++) {
+                if (j != i)
+                    helper[new_size++] = words[j];
+            }
+
+            if (find_substring_helper(s + word_len, helper, new_size, word_len))
+                return true;
+        }
+    }
+
+    return false;
+}
---
+++ substring/substring.h
@@ -0,0 +1,11 @@
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+bool find_substring_helper(char const *s, char const **words, int nb_words, int word_len);
+
+#endif /* SUBSTRING_H */
